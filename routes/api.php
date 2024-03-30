7b1648b2bf879c94ee657b156a5687464e1f32d4
Bayekula-hb <hobedbayekula@gmail.com>
2024-03-30 12:26:29 +0100

    Feat: add api dependacy

---
+++ routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::get('/', function (Request $request){
+    return response()->json([
+        'message' => 'Bienvenu sur l\'api de Raiden Express Solution',
+        'version' => 'C\'est la version beta de cet API merci',
+        'Developpeur' => 'Developper par hobedbayekula@gmail.com',
+    ]);
+});
+// ->middleware('cors');
