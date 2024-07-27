5ef546daebd8c1842e8e87c937ab73001b4f5086
Bayekula-hb <hobedbayekula@gmail.com>
2024-07-27 12:51:24 +0100

    Refactor: add soft deletes in all models

--- app/Models/historicInventoryDrink.php
+++ app/Models/historicInventoryDrink.php
@@ -6,13 +6,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class historicInventoryDrink extends Model
 {
-    use HasFactory;
+    use HasFactory;    
+    use SoftDeletes;
 
-    
-    use HasFactory;
     /**
      * The attributes that are mass assignable.
      *
